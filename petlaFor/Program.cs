18bd1639a7b3e7cff932d2d49a34e20fecffb04d
MateuszJas <pphu.jaskolka@hotmail.com>
2022-11-10 22:32:47 +0100

    Dodaj pliki projektów.

---
+++ petlaFor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petlaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+            int i = 0;
+            int l = 0;
+            while (a < 100)
+            {
+                Console.WriteLine("Wpisz liczbę");
+                l = System.Convert.ToInt16(Console.ReadLine());
+                a = a + l; 
+                i ++;
+            } 
+
+            Console.WriteLine("Doliczyłeś do 100. " + "Dodałeś "+ i + " liczb");
+            Console.ReadKey();
+        }
+            
+        
+
+    }
+}
